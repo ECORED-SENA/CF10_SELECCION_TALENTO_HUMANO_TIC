aa0b95ba1af8411f4330ab4b6897b690a680955c
Andrea Botello <andrea.botellor@misena.edu.co>
2022-04-19 12:27:54 -0500

    updated

--- src/views/Tema7.vue
+++ src/views/Tema7.vue
@@ -9,9 +9,94 @@
       h1 Evaluación del reclutamiento de personal en las organizaciones (Buenas prácticas)
 
 
-      
+    .row.bg-tema7.mb-4
+      .col-lg-12.px-lg-5.py-lg-4.px-4.py-0
+        .row.align-items-center.p-2.py-0.p-md-0.py-md-0
+          .col-md-4(data-aos="fade-right").mb-4.mb-md-0
+            img(src='@/assets/curso/temas/tema7/img.png' alt='Imagen decorativa')
+          .col-md-8(data-aos="fade-left").mb-4.mb-md-0
+            .row.align-items-start
+              .col-auto.disp-none
+                img(src='@/assets/curso/temas/deco-azul-blanco.svg' alt='Decoración')
+              .col
+                p.text-azul Hoy día en pleno siglo XXI, con la incorporación de las tecnologías de la información y las comunicaciones en las empresas que se direccionan hacia una transformación digital, se hace necesario revisar buenas prácticas en el proceso de contratación del talento humano, al igual que conocer las diferentes herramientas tecnológicas que se están usando al realizar dicho proceso. A continuación, se mencionan algunas de estas que han sido sintetizadas mediante un proceso de investigación sobre el tema.
+            p Una vez se tenga el anuncio y se haga la publicación de la vacante por los diferentes medios y fuentes de reclutamiento, el paso a seguir es la recolección de las hojas de vida de acuerdo con los medios establecidos, tales como:
+            .row.space-3columns.justify-content-center  
+              .col-md-4.mb-3.mx-2
+                .row.align-items-center.bg-2.br-10.bx-shadow
+                  .col-auto.bx-shadow.bg-white.br-10.py-2.px-3
+                    img(src='@/assets/curso/temas/tema7/ico1.svg' alt='Icono')
+                  .col.text-center
+                    h5.mb-0 Correo electrónico  
+              .col-md-4.mb-3.mx-2
+                .row.align-items-center.bg-2.br-10.bx-shadow
+                  .col-auto.bx-shadow.bg-white.br-10.py-2.px-3
+                    img(src='@/assets/curso/temas/tema7/ico2.svg' alt='Icono')
+                  .col.text-center
+                    h5.mb-0 Página web de la empresa
+              .col-md-4.mb-3.mx-2
+                .row.align-items-center.bg-2.br-10.bx-shadow
+                  .col-auto.bx-shadow.bg-white.br-10.py-2.px-3
+                    img(src='@/assets/curso/temas/tema7/ico3.svg' alt='Icono')
+                  .col.text-center.px-2
+                    h5.mb-0 Búsquedas en los bancos de hojas de vida, etc           
+
+    .row.align-items-center.mb-4(data-aos="fade")
+      .col-auto.disp-none
+        img(src='@/assets/curso/temas/deco-horiz-azul.svg' alt='Decoración')
+      .col
+        h5.mb-0 Antes de iniciar el análisis, se deben definir criterios de preselección por lo que se propone la construcción de una lista de chequeo. 
     
-    
+    .row.bg-tema7-2.mb-4
+      .col-lg-12.px-lg-5.px-4.py-4
+        .row.p-2.py-4.p-md-0.py-md-0
+          .col-md-6(data-aos="fade-right").mb-4.mb-md-0
+            p Para complementar la información, lo invitamos a leer el artículo Cómo elaborar una lista de chequeo, el cual se encuentra en el material complementario.
+            .cajon.color-acento-botones.neg--right.p-4.mb-4
+              p.mb-0 A partir de esta Lista de Chequeo se pueden analizar y evaluar las hojas de vida según las competencias y requisitos definidos con anterioridad para el cargo.
+            .row.mb-4
+              .col-auto.disp-none
+                img(src='@/assets/curso/temas/deco-horiz.svg' alt='Decoración')
+              .col
+                p.mb-0.text-morado Aquí cabe aclarar que el proceso de Reclutamiento y selección de Talento Humano, integra tres componentes básicos: 
+            .row.space-3columns.justify-content-center
+              .col-md-4.text-center.tarjeta.bx-shadow.bg-white.p-4.px-2.mb-3.mx-1
+                .row.justify-content-center.mb-3
+                  .col-5.col-md-6
+                    img(src='@/assets/curso/temas/tema7/tarj-ico1.svg' alt='AvatarTop')
+                h5.mb-0 Direccionamiento estratégico
+              .col-md-4.text-center.tarjeta.bx-shadow.bg-white.p-4.px-2.mb-3.mx-1
+                .row.justify-content-center.mb-3
+                  .col-5.col-md-6
+                    img(src='@/assets/curso/temas/tema7/tarj-ico2.svg' alt='AvatarTop')
+                h5.mb-0 Administración por procesos
+              .col-md-4.text-center.tarjeta.bx-shadow.bg-white.p-4.px-2.mb-3.mx-1
+                .row.justify-content-center.mb-3
+                  .col-5.col-md-6
+                    img(src='@/assets/curso/temas/tema7/tarj-ico3.svg' alt='AvatarTop')
+                h5.mb-0 Gestión de TH
+          .col-md-6(data-aos="fade-left")
+            img(src='@/assets/curso/temas/tema7/img2.png' alt='Imagen decorativa')
+
+    .row.justify-content-center.mb-5(data-aos="fade-down")
+      .col-md-11.bg-1.px-4.py-3
+        .row
+          .col-auto.disp-none
+            img(src='@/assets/curso/temas/deco-azul-blanco.svg' alt='Decoración')
+          .col
+            p.mb-0 Tener en cuenta los tres elementos anteriormente mencionados, hace que el proceso sea coherente con el planeamiento o requerimientos para el cargo. Como se ha dicho anteriormente, este consta de una serie de etapas que llevan a la toma de decisiones y anticipan la posibilidad de encontrar personas que no se adapten al desempeño laboral esperado.  
+
+    p.mb-5(data-aos="fade") Es importante resaltar y diferenciar las responsabilidades del área de talento humano y del área solicitante del proceso, es decir, el cliente interno dentro del proceso de reclutamiento.
+
+    .row.justify-content-center
+      .col-md-11
+        .titulo-sexto.color-naranja(data-aos="fade-right") 
+          p.mb-0.text-small <b>Tabla 1.</b> Responsabilidades
+        figure.mb-5(data-aos="fade-down") 
+          img.mb-4(src='@/assets/curso/temas/tema7/tabla.svg', alt='Responsabilidades')
+
+
+
 
 </template>
 
